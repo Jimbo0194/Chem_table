71fdaf6ae8a4eab263be41e479e8c8230b9e1dd9
Josué <josuji-alfa@hotmail.com>
2021-08-10 17:06:58 -0600

    New project

--- elementos.py
+++ elementos.py
@@ -1,6 +1,6 @@
 class elemento:
 
-    def __init__(self, nombre, tipo, siglas, pesoAtomico, clasificacion, valencia, oxidacion):
+    def __init__(self, nombre, tipo, siglas, pesoAtomico, clasificacion, valencia, oxidacion, posicion):
         self.nombre = nombre
         self.tipo = tipo
         self.siglas = siglas
@@ -8,3 +8,4 @@ class elemento:
         self.clasificacion = clasificacion
         self.valencia = valencia
         self.oxidacion = oxidacion
+        self.posicion = posicion
